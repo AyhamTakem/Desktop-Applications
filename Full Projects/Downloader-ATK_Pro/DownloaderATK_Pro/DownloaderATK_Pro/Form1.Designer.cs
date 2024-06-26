4af572fb8cab6c96d67b10dbcdc6cee709937605
Ayham Takem <145565732+AyhamTakem@users.noreply.github.com>
2024-06-26 07:23:29 +0300

    Add files via upload

---
+++ Full Projects/Downloader-ATK_Pro/DownloaderATK_Pro/DownloaderATK_Pro/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace DownloaderATK_Pro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_url = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblsatus = new System.Windows.Forms.Label();
+            this.lblpersent = new System.Windows.Forms.Label();
+            this.btn_pause = new System.Windows.Forms.Button();
+            this.btn_resume = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbdownloaded = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbspeed = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(362, 170);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "&Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL:";
+            // 
+            // txt_url
+            // 
+            this.txt_url.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_url.Location = new System.Drawing.Point(63, 59);
+            this.txt_url.Name = "txt_url";
+            this.txt_url.Size = new System.Drawing.Size(536, 22);
+            this.txt_url.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(63, 128);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(536, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Status:";
+            // 
+            // lblsatus
+            // 
+            this.lblsatus.AutoSize = true;
+            this.lblsatus.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsatus.Location = new System.Drawing.Point(77, 101);
+            this.lblsatus.Name = "lblsatus";
+            this.lblsatus.Size = new System.Drawing.Size(25, 14);
+            this.lblsatus.TabIndex = 1;
+            this.lblsatus.Text = "???";
+            // 
+            // lblpersent
+            // 
+            this.lblpersent.AutoSize = true;
+            this.lblpersent.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpersent.Location = new System.Drawing.Point(574, 111);
+            this.lblpersent.Name = "lblpersent";
+            this.lblpersent.Size = new System.Drawing.Size(26, 14);
+            this.lblpersent.TabIndex = 1;
+            this.lblpersent.Text = "0%";
+            // 
+            // btn_pause
+            // 
+            this.btn_pause.Location = new System.Drawing.Point(443, 170);
+            this.btn_pause.Name = "btn_pause";
+            this.btn_pause.Size = new System.Drawing.Size(75, 23);
+            this.btn_pause.TabIndex = 0;
+            this.btn_pause.Text = "&Pause";
+            this.btn_pause.UseVisualStyleBackColor = true;
+            this.btn_pause.Click += new System.EventHandler(this.btn_pause_Click);
+            // 
+            // btn_resume
+            // 
+            this.btn_resume.Location = new System.Drawing.Point(524, 170);
+            this.btn_resume.Name = "btn_resume";
+            this.btn_resume.Size = new System.Drawing.Size(75, 23);
+            this.btn_resume.TabIndex = 0;
+            this.btn_resume.Text = "&Resume";
+            this.btn_resume.UseVisualStyleBackColor = true;
+            this.btn_resume.Click += new System.EventHandler(this.btn_resume_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 14);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Downloaded:";
+            // 
+            // lbdownloaded
+            // 
+            this.lbdownloaded.AutoSize = true;
+            this.lbdownloaded.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbdownloaded.Location = new System.Drawing.Point(110, 204);
+            this.lbdownloaded.Name = "lbdownloaded";
+            this.lbdownloaded.Size = new System.Drawing.Size(34, 14);
+            this.lbdownloaded.TabIndex = 4;
+            this.lbdownloaded.Text = "0 MB";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(48, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 14);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Speed:";
+            // 
+            // lbspeed
+            // 
+            this.lbspeed.AutoSize = true;
+            this.lbspeed.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbspeed.Location = new System.Drawing.Point(110, 232);
+            this.lbspeed.Name = "lbspeed";
+            this.lbspeed.Size = new System.Drawing.Size(44, 14);
+            this.lbspeed.TabIndex = 5;
+            this.lbspeed.Text = "0 MB/s";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(617, 257);
+            this.Controls.Add(this.lbspeed);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbdownloaded);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.txt_url);
+            this.Controls.Add(this.lblpersent);
+            this.Controls.Add(this.lblsatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_resume);
+            this.Controls.Add(this.btn_pause);
+            this.Controls.Add(this.btn_start);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ATK Downloader Pro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_url;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblsatus;
+        private System.Windows.Forms.Label lblpersent;
+        private System.Windows.Forms.Button btn_pause;
+        private System.Windows.Forms.Button btn_resume;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbdownloaded;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbspeed;
+    }
+}
+
---
+++ Full Projects/Downloader-ATK_Pro/DownloaderATK_Pro/DownloaderATK_Pro/Form1.cs
@@ -0,0 +1,61 @@
+﻿using AltoHttp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DownloaderATK_Pro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        HttpDownloader httpDownloader;
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            httpDownloader = new HttpDownloader(txt_url.Text, $"{Application.StartupPath}\\{Path.GetFileName(txt_url.Text)}");
+            httpDownloader.DownloadCompleted += HttpDownloader_DownloadCompleted;
+            httpDownloader.ProgressChanged += HttpDownloader_ProgressChanged;
+            httpDownloader.Start();
+        }
+
+        private void HttpDownloader_ProgressChanged(object sender, AltoHttp.ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = (int)e.Progress;
+            lblpersent.Text = $"{e.Progress.ToString("0.00")} %";
+            lbspeed.Text = string.Format("{0} MB/s", (e.SpeedInBytes / 1024d / 1024d).ToString("0.00"));
+            lbdownloaded.Text = string.Format("{0} MB/s", (httpDownloader.TotalBytesReceived / 1024d / 1024d).ToString("0.00"));
+            lblsatus.Text = "Downloading...";
+        }
+
+        private void HttpDownloader_DownloadCompleted(object sender, EventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                lblsatus.Text = "Finish !";
+                lblpersent.Text = "100 %";
+            });
+        }
+
+        private void btn_pause_Click(object sender, EventArgs e)
+        {
+            if(httpDownloader != null)
+            httpDownloader.Pause();
+        }
+
+        private void btn_resume_Click(object sender, EventArgs e)
+        {
+            if (httpDownloader != null)
+                httpDownloader.Resume();
+        }
+    }
+}
